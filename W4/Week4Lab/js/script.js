060dcd344e98317b1e36c7b2207a7ac2436323cc
JakeFontaine821 <jfontaine@email.neit.edu>
2020-10-29 11:05:19 -0400

    End of class Day2 Week4

--- W4/Week4Lab/js/script.js
+++ W4/Week4Lab/js/script.js
@@ -3,12 +3,34 @@
 var c = document.querySelector('canvas');
 var ctx = c.getContext('2d');
 
-function draw(){
+var mario = new Image(); 
+mario.src = 'Images/mario.png'
 
+// this starts timer for animation
+var timer = requestAnimationFrame(draw);
+
+var x = 0;
+
+function draw(){
+    //this will call the animation again
+    timer = requestAnimationFrame(draw);
+    //clear the screen
+    ctx.clearRect(0,0, c.width, c.height);
+
+    
+    x++;
+    //conditional statement
+    if(x > c.width){
+        x = -200;
+    }
+    
+
+    /*
     //draw a box
     ctx.fillStyle = 'red';
-    ctx.fillRect(20, 20, 20, 20);
-    
+    ctx.fillRect(200, 100, 200, 200);
+
+    //draw everything to the screen    
     //draw a line 1
     ctx.strokeStyle = 'green';
     ctx.moveTo(0,0);
@@ -21,12 +43,27 @@ function draw(){
     ctx.stroke();
 
     //draw a circle
+    ctx.lineWidth = 5;
     ctx.fillStyle = 'Blue';
     ctx.strokeStyle = 'Orange';
     ctx.beginPath();
     ctx.arc(c.width/2, c.height/2, 20, 0, 2*Math.PI, false);
+
     ctx.stroke();
     ctx.fill();
+    */
+
+    //draws mario
+    ctx.drawImage(mario, x, 100, 200, 200);
+
+    //draws text
+    ctx.lineWidth = 1;
+    ctx.font = "50px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText("Week 4 'Lab'", c.width/2, 50);
+    ctx.strokeText("Week 4 'Lab'", c.width/2, 50);
+
+    console.log("We are animating");
 
 }
 
